092fedd43c91d90b445b7d766acb91adc0d25dcf
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2025-12-22 11:43:52 +0800

    Create test_ascend_lts_qwen3_235b.py

---
+++ 1222/test_ascend_lts_qwen3_235b.py
@@ -0,0 +1,232 @@
+import os
+import subprocess
+import sys
+import datetime
+
+import psutil
+import socket
+import unittest
+from types import SimpleNamespace
+
+from sglang.srt.utils import kill_process_tree
+from sglang.test.few_shot_gsm8k import run_eval
+from sglang.test.test_utils import (
+    DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+    DEFAULT_URL_FOR_TEST,
+    CustomTestCase,
+    popen_launch_server,
+)
+
+def get_nic_name():
+    for nic, addrs in psutil.net_if_addrs().items():
+        for addr in addrs:
+            if addr.family == socket.AF_INET and addr.address.startswith("192."):
+                print("The nic name matched is {}".format(nic))
+                return nic
+    return None
+
+NIC_NAME = "lo" if get_nic_name() == None else get_nic_name()
+
+# QWEN3_32B_MODEL_PATH = "/root/.cache/modelscope/hub/models/aleoyang/Qwen3-32B-w8a8-MindIE"
+QWEN3_235B_MODEL_PATH = "/root/.cache/modelscope/hub/models/vllm-ascend/Qwen3-235B-A22B-W8A8"  #
+QWEN3_235B_A22B_EAGLE_MODEL_PATH = "/root/.cache/modelscope/hub/models/Qwen/Qwen3-235B-A22B-Eagle3"
+QWEN3_235B_OTHER_ARGS = [
+        "--trust-remote-code",
+        "--nnodes",
+        "1",
+        "--node-rank",
+        "0",
+        "--attention-backend",
+        "ascend",
+        "--device",
+        "npu",
+        "--quantization",
+        "modelslim",
+        "--max-running-requests",
+        "480",
+        "--context-length",
+        "8192",
+        "--dtype",
+        "bfloat16",
+        "--chunked-prefill-size",
+        "-1",
+        "--max-prefill-tokens",
+        "16384",
+        "--speculative-draft-model-quantization",
+        "unquant",
+        "--speculative-algorithm",
+        "EAGLE3",
+        "--speculative-draft-model-path",
+        QWEN3_235B_A22B_EAGLE_MODEL_PATH,
+        "--speculative-num-steps",
+        "3",
+        "--speculative-eagle-topk",
+        "1",
+        "--speculative-num-draft-tokens",
+        "4",
+        "--disable-radix-cache",
+        "--moe-a2a-backend",
+        "deepep",
+        "--deepep-mode",
+        "auto",
+        "--tp",
+        "16",
+        "--dp-size",
+        "16",
+        "--enable-dp-attention",
+        "--enable-dp-lm-head",
+        "--mem-fraction-static",
+        "0.78",
+        "--cuda-graph-bs",
+        "6",
+        "8",
+        "10",
+        "12",
+        "15",
+        "18",
+        "28",
+        "30",
+]
+
+QWEN3_235B_ENVS = {
+    "SGLANG_SET_CPU_AFFINITY": "1",
+    "PYTORCH_NPU_ALLOC_CONF": "expandable_segments:True",
+    "SGLANG_DISAGGREGATION_BOOTSTRAP_TIMEOUT": "600",
+    "HCCL_BUFFSIZE": "1600",
+    "HCCL_SOCKET_IFNAME": NIC_NAME,
+    "GLOO_SOCKET_IFNAME": NIC_NAME,
+    "HCCL_OP_EXPANSION_MODE": "AIV",
+    "SGLANG_ENABLE_OVERLAP_PLAN_STREAM": "1",
+    "SGLANG_ENABLE_SPEC_V2": "1",
+    "SGLANG_SCHEDULER_DECREASE_PREFILL_IDLE": "1",
+    "ENABLE_PROFILING": "1",
+}
+
+
+def run_command(cmd, shell=True):
+    try:
+        result = subprocess.run(
+            cmd, shell=shell, capture_output=True, text=True, check=False
+        )
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"command error: {e}")
+        return None
+
+def run_bench_serving(host, port, dataset_name="random", dataset_path="", request_rate=8.0, max_concurrency=8, num_prompts=32, input_len=1024, output_len=1024,
+                      random_range_ratio=1.0):
+    command = (f"python3 -m sglang.bench_serving --backend sglang --host {host} --port {port} --dataset-name {dataset_name} --dataset-path {dataset_path} --request-rate {request_rate} "
+               f"--max-concurrency {max_concurrency} --num-prompts {num_prompts} --random-input-len {input_len} "
+               f"--random-output-len {output_len} --random-range-ratio {random_range_ratio}")
+    print(f"command:{command}")
+    metrics = run_command(f"{command} | tee ./bench_log.txt")
+    return metrics
+
+class TestLTSQwen332B(CustomTestCase):
+    model = QWEN3_235B_MODEL_PATH
+    dataset_name = "random"
+    dataset_path = "/tmp/ShareGPT_V3_unfiltered_cleaned_split.json"  # the path of test dataset
+    other_args = QWEN3_235B_OTHER_ARGS
+    timeout = DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH * 10
+    envs = QWEN3_32B_ENVS
+    request_rate = 5.5
+    max_concurrency = 480
+    num_prompts = int(max_concurrency) * 4
+    input_len = 2048
+    output_len = 2048
+    random_range_ratio = 0.5
+    ttft = 10000
+    tpot = 50
+    output_token_throughput = 8314
+    accuracy = 0.80
+
+    print("Nic name: {}".format(NIC_NAME))
+
+    @classmethod
+    def setUpClass(cls):
+        cls.base_url = DEFAULT_URL_FOR_TEST
+        env = os.environ.copy()
+        env.update(cls.envs)
+
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=cls.timeout,
+            other_args=cls.other_args,
+            env=env,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def run_throughput(self):
+        _, host, port = self.base_url.split(":")
+        host = host[2:]
+        metrics = run_bench_serving(
+            host=host,
+            port=port,
+            dataset_name=self.dataset_name,
+            dataset_path=self.dataset_path,
+            request_rate=self.request_rate,
+            max_concurrency=self.max_concurrency,
+            num_prompts=self.num_prompts,
+            input_len=self.input_len,
+            output_len=self.output_len,
+            random_range_ratio=self.random_range_ratio,
+        )
+        print("metrics is " + str(metrics))
+        res_ttft = run_command(
+            "cat ./bench_log.txt | grep 'Mean TTFT' | awk '{print $4}'"
+        )
+        res_tpot = run_command(
+            "cat ./bench_log.txt | grep 'Mean TPOT' | awk '{print $4}'"
+        )
+        res_output_token_throughput = run_command(
+            "cat ./bench_log.txt | grep 'Output token throughput' | awk '{print $5}'"
+        )
+
+    def run_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=1319,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        metrics = run_eval(args)
+        self.assertGreater(
+            metrics["accuracy"],
+            self.accuracy,
+            f'Accuracy of {self.model} is {str(metrics["accuracy"])}, is lower than {self.accuracy}',
+        )
+
+    def test_lts_qwen3_235b(self):
+        i = 0
+        while True:
+            i = i + 1
+            time_str_1 = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"=============={time_str_1}  Execute the {i}-th long-term stability test==============")
+            self.run_throughput()
+            self.run_gsm8k()
+
+
+if __name__ == "__main__":
+    time_str = datetime.datetime.now().strftime("%Y%m%d%H%M")
+    log_file = "/tmp/lts_test_qwen3_235b_" + time_str + ".log"
+
+    with open(log_file, 'w', encoding="utf-8") as f:
+        original_stdout = sys.stdout
+        original_stderr = sys.stderr
+        sys.stdout = f
+        sys.stderr = f
+
+        try:
+            unittest.main(verbosity=2)
+        finally:
+            sys.stdout = original_stdout
+            sys.stderr = original_stderr
+
+    print(f"Test log saved to {log_file}")
