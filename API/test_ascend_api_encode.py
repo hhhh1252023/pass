11a1c5177feac0c4ac2fd54a5d9d4b94f7b8dce4
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-10 16:23:25 +0800

    Update test_ascend_api_encode.py

--- API/test_ascend_api_encode.py
+++ API/test_ascend_api_encode.py
@@ -35,63 +35,44 @@ class TestAscendApi(CustomTestCase):
     @classmethod
     def tearDownClass(cls):
         kill_process_tree(cls.process.pid)
-
-     def test_api_encode(self):
+        
+    def test_api_encode_01(self):
         response = requests.post(
             f"{DEFAULT_URL_FOR_TEST}/encode",
             json={
                 "rid": 1,
-                "http_worker_ipc": "127.0.0.1:8888"
-                "validation_time": 5,
-                "received_time":20,
-                "received_time_perf": 5,
-                "
+                "text": "what is the capital of France",
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 200,    
+                },
                 
-              
             },
         )
         self.assertEqual(response.status_code, 200)
         print(response.json())
         self.assertEqual(response.json()['rid'], 1)
+        self.assertEqual(response.json()['sampling_params']['temperature'], 0)
 
-
-    def test_api_clear_hicache_storage_backend(self):
-        response = requests.get(f"{DEFAULT_URL_FOR_TEST}/clear_hicache_storage_backend")
-        self.assertEqual(response.status_code, 200)
-        print(response.json())
-        self.assertEqual(response.json(), "Hierarchical cache storage backend cleared.")
-
-    def test_api_set_internal_state(self):
-        response = requests.POST(
-            f"{DEFAULT_URL_FOR_TEST}/set_internal_state",
-            json={
-                "server_args": {"pp_max_micro_batch_size": 8}
-            },
-        )
-        self.assertEqual(response.status_code, 200)
-        print(response.json())
-        self.assertEqual(response.json(), "Hierarchical cache storage backend cleared.")
-
-    def test_api_configure_logging(self):
-        response = requests.get(f"{DEFAULT_URL_FOR_TEST}/configure_logging")
+    def test_api_encode_02(self):
         response = requests.post(
-            f"{DEFAULT_URL_FOR_TEST}/configure_logging",
+            f"{DEFAULT_URL_FOR_TEST}/encode",
             json={
-                "log_requests": "True",
-                "log_requests": true, 
-                "log_requests_level": 3, 
-                "dump_requests_folder":  "/home/l30079981/test/dump_requests_folder",
-                "dump_requests_threshold": 3, 
-                "crash_dump_folder": "/home/l30079981/test/crash_dump_folder"
+                "rid": 1,
+                "input_ids": 
+                "sampling_params": {
+                    "temperature": 0,
+                    "max_new_tokens": 200,    
+                },
+                
             },
         )
         self.assertEqual(response.status_code, 200)
         print(response.json())
-        self.assertEqual(response.json()['log_requests'], True)
-        self.assertEqual(response.json()['log_requests_level'], "3")
-        self.assertEqual(response.json()['dump_requests_folder'], "/home/l30079981/test/dump_requests_folder")
-        self.assertEqual(response.json()['dump_requests_threshold'], 3)
-        self.assertEqual(response.json()['crash_dump_folder'], "/home/l30079981/test/crash_dump_folder")
+        self.assertEqual(response.json()['rid'], 1)
+        self.assertEqual(response.json()['sampling_params']['temperature'], 0)
+        
+
 
 if __name__ == "__main__":
 
