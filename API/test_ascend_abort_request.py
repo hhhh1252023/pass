5e341c7029065eadb6528a9a38071550dcb75191
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-10 16:38:00 +0800

    Update test_ascend_abort_request.py

--- API/test_ascend_abort_request.py
+++ API/test_ascend_abort_request.py
@@ -14,7 +14,7 @@ from sglang.test.test_utils import (
     popen_launch_server,
 )
 DEFAULT_URL_FOR_TEST="http://127.0.0.1:2345"
-responses = []
+response = []
 def send_requests(url, **kwargs):
     data = json.dumps(kwargs)
     response = requests.post('http://127.0.0.1:2345' + url, json=data)
