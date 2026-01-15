abf72f145e79bd7ccfab74515795f7ce2d4eceab
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-01-15 21:21:11 +0800

    Update test_ascend_deepep_auto_deepseek_R1.py

--- deepep/test_ascend_deepep_auto_deepseek_R1.py
+++ deepep/test_ascend_deepep_auto_deepseek_R1.py
@@ -61,7 +61,7 @@ class TestPureTP(CustomTestCase):
                 "--moe-a2a-backend",
                 "deepep",
                 "--deepep-mode",
-                "auto",
+                "normal",
                 "--enable-dp-attention",
                 "--dp-size",
                 "4",
