af3ebbd454743bb855b0b8dfdf00774f57d93cd9
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-04-11 16:59:14 +0800

    Create install.sh

---
+++ newyear/debug/install.sh
@@ -0,0 +1,13 @@
+apt update && apt install -y \
+build-essential \
+python3-dev \
+python3-setuptools \
+make \
+cmake \
+ffmpeg \
+libavcodec-dev \
+libavfilter-dev \
+libavformat-dev \
+libavutil-dev \
+libswscale-dev \
+libavdevice-dev
