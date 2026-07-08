3ddf72a88246db5470e0f1ab948586634f5ea1d0
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-07-08 18:10:36 +0800

    Create clone.sh

---
+++ newyear/clone.sh
@@ -0,0 +1,5 @@
+git lfs install
+# 第一个LoRA
+git clone https://ghproxy.com/https://huggingface.co/nissenj/Qwen3-4B-lora-v2
+# 第二个网文LoRA
+git clone https://ghproxy.com/https://huggingface.co/TanXS/Qwen3-4B-LoRA-ZH-WebNovelty-v0.0
