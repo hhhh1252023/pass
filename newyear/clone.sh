65bb3963543f6e40d0f17c5b3e04b9aaaa1bf5b8
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-07-08 19:07:49 +0800

    Update clone.sh

--- newyear/clone.sh
+++ newyear/clone.sh
@@ -3,3 +3,7 @@ git lfs install
 git clone https://ghproxy.com/https://huggingface.co/nissenj/Qwen3-4B-lora-v2
 # 第二个网文LoRA
 git clone https://ghproxy.com/https://huggingface.co/TanXS/Qwen3-4B-LoRA-ZH-WebNovelty-v0.0
+
+huggingface-cli download nissenj/Qwen3-4B-lora-v2 --resume-download --local-dir-use-symlinks False --local-dir ./Qwen3-4B-lora-v2
+
+huggingface-cli download TanXS/Qwen3-4B-LoRA-ZH-WebNovelty-v0.0 --resume-download --local-dir-use-symlinks False --local-dir ./Qwen3-4B-LoRA-ZH-WebNovelty-v0.0
