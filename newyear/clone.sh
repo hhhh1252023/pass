335efb651d0372751462b25fc50774ebd4199735
hhhh1252023 <139246842+hhhh1252023@users.noreply.github.com>
2026-07-11 16:17:05 +0800

    Update clone.sh

--- newyear/clone.sh
+++ newyear/clone.sh
@@ -7,3 +7,6 @@ git clone https://ghproxy.com/https://huggingface.co/TanXS/Qwen3-4B-LoRA-ZH-WebN
 huggingface-cli download nissenj/Qwen3-4B-lora-v2 --resume-download --local-dir-use-symlinks False --local-dir ./Qwen3-4B-lora-v2
 
 huggingface-cli download TanXS/Qwen3-4B-LoRA-ZH-WebNovelty-v0.0 --resume-download --local-dir-use-symlinks False --local-dir ./Qwen3-4B-LoRA-ZH-WebNovelty-v0.0
+mkdir -p ~/.cache/datasets
+wget -O ~/.cache/datasets/hellaswag_val.jsonl \
+  https://raw.githubusercontent.com/rowanz/hellaswag/master/data/hellaswag_val.jsonl
